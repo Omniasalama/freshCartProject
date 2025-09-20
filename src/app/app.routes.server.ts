618038625a344e24d5779cf220a680236c6a87b1
Omnia Salama <omniamsalama25@gmail.com>
2025-09-21 02:06:47 +0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'address/:cartId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server,
+  },
 ];
